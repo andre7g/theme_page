8e4b8acb5d01560a99814776d6cc9932aeca599a
jgsamayoa <jgsamayoa@gmail.com>
2021-10-14 18:44:28 -0600

    templante

---
+++ zero-pro-theme-v2/theme/src/views/sections/NewsletterAlt.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-theme-provider dark>
+    <base-section
+      id="map"
+      class="accent"
+      space="56"
+    >
+      <base-title
+        space="8"
+        title="GET OUR NEWSLETTER"
+      />
+
+      <v-responsive
+        class="mx-auto px-4"
+        max-width="400"
+      >
+        <v-text-field
+          background-color="rgba(255, 255, 255, .04)"
+          dark
+          flat
+          hide-details
+          label="Your Email Address"
+          solo
+          tile
+        >
+          <template #append>
+            <v-btn
+              icon
+            >
+              <v-icon>mdi-arrow-right</v-icon>
+            </v-btn>
+          </template>
+        </v-text-field>
+      </v-responsive>
+    </base-section>
+  </v-theme-provider>
+</template>
+
+<script>
+  export default {
+    name: 'SectionNewsletter',
+
+    provide: {
+      heading: { align: 'center' },
+    },
+  }
+</script>
---
+++ zero-pro-theme-v2/theme/src/views/sections/RelatedProjects.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-sheet tile>
+    <v-sheet
+      class="py-3"
+      tile
+    >
+      <base-title
+        align="center"
+        title="RELATED PROJECTS"
+        tag="h2"
+        space="0"
+      />
+    </v-sheet>
+
+    <projects class="py-12 py-md-12" />
+  </v-sheet>
+</template>
+
+<script>
+  export default {
+    name: 'SectionProjects',
+
+    components: {
+      Projects: () => import('@/views/sections/Projects'),
+    },
+  }
+</script>
